1b30b9dd17118ef7c0e42907fa98fdce3ffb0908
gaonkarsahil <sahilgaonkar24@gmail.com>
2020-05-02 09:49:44 +0530

    simple chat using pusher vue . But still incomplete

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/chats', 'ChatController@index');
+Route::get('/messages', 'ChatController@fetchAllMessages');
+Route::post('/messages', 'ChatController@sendMessage');
